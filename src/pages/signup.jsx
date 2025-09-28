86fae1f52a6d6992763c5db6984c5cafd9a471c6
Houston Braga <houstonliverboy123@gmail.com>
2025-09-28 12:41:40 -0300

    feat: ensure that the passwords to be iquals

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -29,28 +29,33 @@ import {} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signUpSchema = z.object({
-  firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório',
-  }),
-  email: z
-    .string()
-    .email({ message: 'O email é inválido' })
-    .trim()
-    .min(1, { message: 'O email é obrigatório' }),
-  password: z
-    .string()
-    .trim()
-    .min(8, { message: 'Senha deve conter no mínimo 8 caracteres' }),
-  passwordConfirmation: z
-    .string()
-    .trim()
-    .min(8, { message: 'Senha deve conter no mínimo 8 caracteres' }),
-  acceptTerms: z.boolean().refine((value) => value === true, {
-    message: 'Precisa aceitar os termos para criação de conta',
-  }),
-})
+const signUpSchema = z
+  .object({
+    firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório',
+    }),
+    email: z
+      .string()
+      .email({ message: 'O email é inválido' })
+      .trim()
+      .min(1, { message: 'O email é obrigatório' }),
+    password: z
+      .string()
+      .trim()
+      .min(8, { message: 'Senha deve conter no mínimo 8 caracteres' }),
+    passwordConfirmation: z
+      .string()
+      .trim()
+      .min(8, { message: 'Senha deve conter no mínimo 8 caracteres' }),
+    acceptTerms: z.boolean().refine((value) => value === true, {
+      message: 'Precisa aceitar os termos para criação de conta',
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senhas não coincidem!',
+    path: ['passwordConfirmation'],
+  })
 
 const SignUpPage = () => {
   const [user, setUser] = useState(null)
