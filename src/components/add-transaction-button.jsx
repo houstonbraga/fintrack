6f1187b69144e1bda91fafcb941471d008575ab7
Houston Braga <houstonliverboy123@gmail.com>
2025-10-10 14:32:55 -0300

    feat: add transaction button with zod and react hook form, without actions to api

---
+++ src/components/add-transaction-button.jsx
@@ -0,0 +1,190 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  PiggyBankIcon,
+  PlusIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+} from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { NumericFormat } from 'react-number-format'
+import z from 'zod'
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import { Button } from './ui/button'
+import { DatePicker } from './ui/date-picker'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form'
+import { Input } from './ui/input'
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: 'O nome deve conter no mínimo 1 caractere!',
+  }),
+  amount: z.number({
+    required_error: 'O valor precisa ser preenchido!',
+  }),
+  date: z.date(),
+  type: z.enum(['EARNING', 'EXPENSE', 'INVESTMENT']),
+})
+
+const AddTransactionButton = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      amount: 0,
+      date: new Date(),
+      type: 'EARNING',
+    },
+    shouldUnregister: true,
+  })
+
+  const onSubmit = (data) => {
+    console.log({ data })
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="flex items-center gap-2">
+          <PlusIcon />
+          Nova transação
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar nova transação</DialogTitle>
+          <DialogDescription>Insira os dados abaixo</DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Nome da transação" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <NumericFormat
+                      placeholder="Digite o valor"
+                      thousandSeparator="."
+                      allowNegative={false}
+                      decimalSeparator=","
+                      prefix="R$ "
+                      customInput={Input}
+                      {...field}
+                      onChange={() => {}}
+                      onValueChange={(values) =>
+                        field.onChange(values.floatValue)
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Data</FormLabel>
+                  <FormControl>
+                    <DatePicker {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="grid grid-cols-3 gap-4">
+                      <Button
+                        type="button"
+                        variant={
+                          field.value === 'EARNING' ? 'secondary' : 'outline'
+                        }
+                        onClick={() => field.onChange('EARNING')}
+                      >
+                        <TrendingUpIcon className="text-green-500" />
+                        Ganho
+                      </Button>
+                      <Button
+                        type="button"
+                        variant={
+                          field.value === 'EXPENSE' ? 'secondary' : 'outline'
+                        }
+                        onClick={() => field.onChange('EXPENSE')}
+                      >
+                        <TrendingDownIcon className="text-red-500" />
+                        Gasto
+                      </Button>
+                      <Button
+                        type="button"
+                        variant={
+                          field.value === 'INVESTMENT' ? 'secondary' : 'outline'
+                        }
+                        onClick={() => field.onChange('INVESTMENT')}
+                      >
+                        <PiggyBankIcon className="text-blue-500" />
+                        Investimento
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="sm:space-x-4">
+              <DialogClose className="w-full" asChild>
+                <Button variant="outline">Cancelar</Button>
+              </DialogClose>
+              <Button className="w-full" type="submit">
+                Adicionar
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default AddTransactionButton
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,9 @@
-import { PlusIcon } from 'lucide-react'
 import { Navigate } from 'react-router'
 
+import AddTransactionButton from '@/components/add-transaction-button'
 import Balance from '@/components/balance'
 import DateSelection from '@/components/date-select'
 import Header from '@/components/Header'
-import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/contexts/auth'
 
 const HomePage = () => {
@@ -24,10 +23,7 @@ const HomePage = () => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-2">
             <DateSelection />
-            <Button className="flex items-center gap-2">
-              <PlusIcon />
-              Nova transação
-            </Button>
+            <AddTransactionButton />
           </div>
         </div>
         <div className="grid grid-cols-[2fr_1fr] gap-4">
