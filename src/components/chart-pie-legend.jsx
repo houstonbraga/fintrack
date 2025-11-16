aa294e2916cdeff27ea1ba3314fdc4772b7711f4
Houston Braga <houstonliverboy123@gmail.com>
2025-11-16 01:39:05 -0300

    feat: add actions to chart pie legend component with useGetBalance hook

--- src/components/chart-pie-legend.jsx
+++ src/components/chart-pie-legend.jsx
@@ -1,5 +1,7 @@
+import { useSearchParams } from 'react-router'
 import { Pie, PieChart } from 'recharts'
 
+import { useGetBalance } from '@/api/hooks/user'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   ChartContainer,
@@ -7,31 +9,49 @@ import {
   ChartLegendContent,
 } from '@/components/ui/chart'
 
-const chartData = [
-  { browser: 'ganhos', visitors: 275, fill: 'hsl(var(--primary-green))' },
-  { browser: 'gastos', visitors: 200, fill: 'hsl(var(--primary-red))' },
-  { browser: 'investimentos', visitors: 187, fill: 'hsl(var(--primary-blue))' },
-]
-
-const chartConfig = {
-  visitors: {
-    label: 'Visitors',
-  },
-  ganhos: {
-    label: 'Ganhos',
-    color: 'hsl(var(--chart-1))',
-  },
-  gastos: {
-    label: 'Gastos',
-    color: 'hsl(var(--chart-2))',
-  },
-  investimentos: {
-    label: 'Investimentos',
-    color: 'hsl(var(--chart-3))',
-  },
-}
-
 export function ChartPieLegend() {
+  const [searchParams] = useSearchParams()
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+
+  const { data } = useGetBalance({ from, to })
+
+  const chartData = [
+    {
+      browser: 'ganhos',
+      visitors: Number(data?.earnings || 0),
+      fill: 'hsl(var(--primary-green))',
+    },
+    {
+      browser: 'gastos',
+      visitors: Number(data?.expenses || 0),
+      fill: 'hsl(var(--primary-red))',
+    },
+    {
+      browser: 'investimentos',
+      visitors: Number(data?.investments || 0),
+      fill: 'hsl(var(--primary-blue))',
+    },
+  ]
+
+  const chartConfig = {
+    visitors: {
+      label: 'Visitors',
+    },
+    ganhos: {
+      label: 'Ganhos',
+      color: 'hsl(var(--chart-1))',
+    },
+    gastos: {
+      label: 'Gastos',
+      color: 'hsl(var(--chart-2))',
+    },
+    investimentos: {
+      label: 'Investimentos',
+      color: 'hsl(var(--chart-3))',
+    },
+  }
+
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
